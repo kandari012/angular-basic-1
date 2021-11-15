786840fb32814afd0de3e5745dcb38e017bf6beb
rahul1 <rahulkandari012@gmail.com>
2021-11-15 16:22:43 +0530

    interpolation

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,28 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-
-//3 ways for selector
-//selector: 'app-test'--use       <app-test></app-test> in app.comp.html
-//selector: '.app-test'         <div class="app-test"><div>   as  class
-//selector: '[app-test]'           <div app-test><div>   as attribute
-
-// instaed of templateUrl we can use template to write html in same file
-//template:"<div>rahul<div>"
-//template:`<div>    backtick to write multiline html
-//rahul
-//<div>`
-
-// styles:`[
-//   div{
-//     color:red
-//   }
-// ]`
+//interpolation  to bind data from class to template
+//{{2+2}},name.lenght,name.uppercase(),name.greetuser
 @Component({
   selector: 'app-test',
-  templateUrl: './test.component.html',
+  template: `<h2>{{ greet() }}</h2>`,
   styleUrls: ['./test.component.css'],
 })
 export class TestComponent implements OnInit {
+  public name = 'rahul kandari';
   constructor() {}
 
   ngOnInit(): void {}
+  greet() {
+    return 'hello Mr. ' + this.name;
+  }
 }
