293ce19c1018e0274fa90f04b74edd0c0940f1f2
rahul1 <rahulkandari012@gmail.com>
2021-11-16 12:43:13 +0530

    pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public name = 'r';
-  public message="";
+  
 }
--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,24 +1,36 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
-//ngFor
+//pipes
 
 @Component({
   selector: 'app-test',
   template: `
-    <h2>hello rk</h2>
-    <button (click)="fireEvent()">click</button>
+    <h2>{{ name | uppercase }}</h2>
+    <h2>{{ name | lowercase }}</h2>
+    <h2>{{ message | titlecase }}</h2>
+    <h2>{{ name | slice: 3:5 }}</h2>
+    <h2>{{ person | json }}</h2>
+    <h2>{{ 5.678 | number: '1.2-3' }}</h2>
+    <h2>{{ 5.678 | number: '3.4-5' }}</h2>
+    <h2>{{ 5.678 | number: '3.1-2' }}</h2>
+
+    <h2>{{ date | date:'short' }}</h2>
+    <h2>{{ date | date:'shortDate' }}</h2>
+    <h2>{{ 0.24 | percent }}</h2>
+    <h2>{{ 0.24 | currency }}</h2>
   `,
   styles: [],
 })
 export class TestComponent implements OnInit {
-  public colors = ['red', 'blue', 'green'];
-  // create ainstance of emitter class
-  @Output() public childEvent = new EventEmitter();
+  public name = 'Codevolution';
+  public message = 'welcome to Codevolution';
+  public person = {
+    firstname: 'rahul',
+    lastname: 'kandari',
+  };
 
+  public date=new Date();
   constructor() {}
 
   ngOnInit(): void {}
-  fireEvent() {
-    this.childEvent.emit('hey code evolution');
-  }
 }
