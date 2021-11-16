1b9e3a88ae14af478190a7ccbaac658d3692ad64
rahul1 <rahulkandari012@gmail.com>
2021-11-16 11:15:26 +0530

    ngSwitch directive

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,29 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
-//ngIf
-//<h2 *ngIf="displayName; else el" >rahul kandari</h2>
-//<ng-template #el>
-//<h2 >name is hidden</h2>
-//</ng-template >
+//ngSwitch
+
 
 @Component({
   selector: 'app-test',
   template: `
-  <div *ngIf="displayName; then thenBlock;else elseBlock"></div>
-  <ng-template #thenBlock>
-  <h2 >rahul k</h2>
-  </ng-template >
-
-  <ng-template #elseBlock>
-  <h2 >name is hidden</h2>
-  </ng-template >
+ <div [ngSwitch]="color">
+ <div *ngSwitchCase="'red'">you picked red color</div>
+ <div *ngSwitchCase="'blue'">you picked blue color</div>
+ <div *ngSwitchCase="'green'">you picked green color</div>
+ <div *ngSwitchDefault>pick again</div>
+ </div>
     `,
   styles: [
     
   ],
 })
 export class TestComponent implements OnInit {
-public displayName=false;
+public color="x";
   
 
   constructor() {}
